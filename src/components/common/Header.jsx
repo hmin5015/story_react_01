d63361bc52f0ad3c152d58a84f1bec3827820079
스터디하자 <hmin5015@gmail.com>
2023-09-19 21:44:18 -0700

    Change UI for header

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -2,7 +2,7 @@ import Typography from "@mui/material/Typography";
 import SearchIcon from "@mui/icons-material/Search";
 import { styled, alpha } from "@mui/material/styles";
 import InputBase from "@mui/material/InputBase";
-import './Header.css';
+import './Header.scss';
 
 const menus = [
   { menuId: 1, menuName: "LOGS" },
@@ -12,21 +12,22 @@ const menus = [
 const Search = styled("div")(({ theme }) => ({
   position: "relative",
   borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.black, 0.05),
+  backgroundColor: alpha(theme.palette.common.white, 100.0),
   "&:hover": {
-    backgroundColor: alpha(theme.palette.common.black, 0.10),
+    backgroundColor: alpha(theme.palette.common.white, 100.0),
   },
   marginRight: theme.spacing(1),
   marginLeft: 0,
   width: "100%",
   [theme.breakpoints.up("sm")]: {
     marginLeft: theme.spacing(2),
+    marginRight: theme.spacing(2),
     width: "auto",
   },
 }));
 
 const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
+  padding: theme.spacing(0, 0.5),
   height: "100%",
   position: "absolute",
   pointerEvents: "none",
@@ -44,8 +45,8 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
   fontSize: "14px",
   "& .MuiInputBase-input": {
     padding: theme.spacing(0, 0, 0, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    paddingLeft: `calc(1em + ${theme.spacing(2.5)})`,
+    paddingRight: `calc(1em + ${theme.spacing(2.5)})`,
     transition: theme.transitions.create("width"),
     width: "100%",
     [theme.breakpoints.up("md")]: {
@@ -57,10 +58,30 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 const Header = () => {
   return (
     <header>
-      <section className="search-section">
-        <Typography variant="subtitle1" fontWeight={600} paddingLeft={2}>
-          {"SPACE"}
-        </Typography>
+      <section className="header-top-section">
+        <div className="header-aside">
+          <div className="header-logo">
+            <Typography variant="overline" fontSize={"10px"} fontWeight={500} lineHeight={2} textTransform={"none"}>
+              {"Sp"}
+            </Typography>
+          </div>
+        </div>
+        <div className="header-info">
+          <ul>
+            {menus.map(menu => (
+              <li key={menu.menuId}>
+                <Typography variant="overline" paddingLeft={2}>{menu.menuName}</Typography>
+              </li>
+            ))}
+          </ul>
+          <div className="header-user-info">
+            <Typography variant="overline" fontSize={"15px"} fontWeight={700} lineHeight={2} textTransform={"none"} letterSpacing={-0.5}>
+              {"Jm"}
+            </Typography>
+          </div>
+        </div>
+      </section>
+      <section className="header-search-section">
         <Search>
           <SearchIconWrapper>
             <SearchIcon />
@@ -71,13 +92,12 @@ const Header = () => {
           />
         </Search>
       </section>
-      <section className="menu-section">
+      <section className="header-filter-section">
         <ul>
-          {menus.map(menu => (
-            <li key={menu.menuId}>
-              <Typography variant="overline" paddingLeft={2}>{menu.menuName}</Typography>
-            </li>
-          ))}
+          <li>전체</li>
+          <li>아이방</li>
+          <li>서재</li>
+          <li>주방</li>
         </ul>
       </section>
     </header>
