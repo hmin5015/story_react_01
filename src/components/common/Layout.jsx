bbe0cfe3191bb22701879fc31f19239004f3fdb1
스터디하자 <hmin5015@gmail.com>
2023-09-25 20:49:24 -0700

    Clean up Layout.jsx and add Layout.scss

--- src/components/common/Layout.jsx
+++ src/components/common/Layout.jsx
@@ -1,10 +1,11 @@
 import { Outlet } from 'react-router-dom'
+import { useRecoilState } from 'recoil'
+import { UserInfoAtom } from '../../recoil/UserInfoAtom'
 import Header from './Header'
 import Nav from './Nav'
 import Typography from "@mui/material/Typography"
-import { useRecoilState } from 'recoil'
-import { UserInfoAtom } from '../../recoil/UserInfoAtom'
 import ArrowBackIosNewOutlinedIcon from '@mui/icons-material/ArrowBackIosNewOutlined'
+import './Layout.scss'
 
 const Layout = () => {
   const [isUserInfoOpen, setIsUserInfoOpen] = useRecoilState(UserInfoAtom)
@@ -12,26 +13,26 @@ const Layout = () => {
   return (
     <>
       {!isUserInfoOpen && 
-      <>
-        <Header />
-        <Nav />
-        <Outlet />
+        <>
+          <Header />
+          <Nav />
+          <Outlet />
         </>
       }
       {isUserInfoOpen && 
-        <section>
-          <article style={{ display: "flex", flexDirection: "column", borderBottom: "1px solid #E5E5E5", padding: "10px" }}>
-            <button style={{ border: "none", backgroundColor: "#FFF", textAlign: "left", padding: "10px 0px" }} onClick={() => setIsUserInfoOpen(false)}>
+        <section className="user-info-section">
+          <article className="user-info-top">
+            <button className="back-button" onClick={() => setIsUserInfoOpen(false)}>
               <ArrowBackIosNewOutlinedIcon fontSize="small" htmlColor="#777" />
             </button>
             <Typography variant="overline" fontSize={"18px"} fontWeight={900} textTransform={"none"} lineHeight={1.5}>
               Account
             </Typography>
           </article>
-          <article style={{ display: "flex", flexDirection: "column", backgroundColor: "#F2F2F2", padding: "10px" }}>
-            <div>
-              <button style={{ backgroundColor: "#00754A", border: "1px solid #00754A", borderRadius: "25px", padding: "10px", color: "#FFF" }}>Join now</button>
-              <button style={{ backgroundColor: "#FFF", border: "1px solid #FFF", borderRadius: "25px", padding: "10px", color: "#00754A" }}>Sign in</button>
+          <article className="user-info-content">
+            <div className="action-button-wrapper">
+              <button className="join-now-button">Join now</button>
+              <button className="sign-in-button">Sign in</button>
             </div>
             <div>
               <div>Notification preference</div>
